896fd8d9f2cb5e4e3c1c490528ade7cb4d6fbc64
wqferr <wqferr@gmail.com>
2017-09-25 13:30:32 +0000

    Remove unnecessary coordlist allocations

--- src/core/transform.c
+++ src/core/transform.c
@@ -21,18 +21,22 @@ void _set_out_pixel(
 
 	size_t len;
 	size_t i;
-	const coord *coord_arr = clist_as_array(clist, &len);
+	const coord *coord_arr;
 	coord c;
 	rgba_pixel in_pixel;
 	rgba_pixel out_pixel;
 
-	for (i = 0; i < len; i++) {
-		c = coord_arr[i];
-		if (rgbaimg_get_pixel(input, c.x, c.y, &in_pixel) == 0) {
-			sum_r += in_pixel.r * in_pixel.a;
-			sum_g += in_pixel.g * in_pixel.a;
-			sum_b += in_pixel.b * in_pixel.a;
-			sum_a += in_pixel.a;
+	if (clist != NULL) {
+		coord_arr = clist_as_array(clist, &len);
+
+		for (i = 0; i < len; i++) {
+			c = coord_arr[i];
+			if (rgbaimg_get_pixel(input, c.x, c.y, &in_pixel) == 0) {
+				sum_r += in_pixel.r * in_pixel.a;
+				sum_g += in_pixel.g * in_pixel.a;
+				sum_b += in_pixel.b * in_pixel.a;
+				sum_a += in_pixel.a;
+			}
 		}
 	}
 
@@ -73,13 +77,7 @@ rgba_image *warp(const rgba_image *input, warp_f transformation) {
 	rgba_image *output;
 
 	rgbaimg_get_dimensions(input, &width, &height);
-	mapping = malloc(height * sizeof(*mapping));
-	for (i0 = 0; i0 < height; i0++) {
-		mapping[i0] = malloc(width * sizeof(**mapping));
-		for (j0 = 0; j0 < width; j0++) {
-			mapping[i0][j0] = clist_create(OUTPUT_MAP_INITIAL_CAP);
-		}
-	}
+	mapping = calloc(height, sizeof(*mapping));
 
 	for (i0 = 0; i0 < height; i0++) {
 		y0 = (double) (height-1 - i0) / (height-1);
@@ -97,6 +95,13 @@ rgba_image *warp(const rgba_image *input, warp_f transformation) {
 				i1 = 0.5 + (height-1) - y1 * (height-1);
 				j1 = 0.5 + x1 * (width-1);
 
+				if (mapping[i1] == NULL) {
+					mapping[i1] = calloc(width, sizeof(**mapping));
+				}
+				if (mapping[i1][j1] == NULL) {
+					mapping[i1][j1] = clist_create(OUTPUT_MAP_INITIAL_CAP);
+				}
+
 				c0.x = j0;
 				c0.y = i0;
 				clist_add(mapping[i1][j1], c0);
@@ -109,7 +114,9 @@ rgba_image *warp(const rgba_image *input, warp_f transformation) {
 		for (j0 = 0; j0 < width; j0++) {
 			_set_out_pixel(input, output, j0, i0, mapping[i0][j0]);
 
-			clist_destroy(mapping[i0][j0]);
+			if (mapping[i0][j0] != NULL) {
+				clist_destroy(mapping[i0][j0]);
+			}
 		}
 		free(mapping[i0]);
 	}
--- src/main.c
+++ src/main.c
@@ -7,14 +7,14 @@
 #include "core/transform.h"
 
 double complex f(double complex z) {
-	return z*z;
+	return (z*z + 1) / 2;
 }
 
 int main(int argc, char const *argv[]) {
 	rgba_image *in_img;
 	rgba_image *out_img;
 
-	png_load_from_file(&in_img, "img/1.png");
+	png_load_from_file(&in_img, "img/5.png");
 	out_img = warp(in_img, &f);
 	png_save_to_file(out_img, "img/out.png");
 	rgbaimg_destroy(in_img);
